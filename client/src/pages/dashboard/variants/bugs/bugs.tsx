827cfc23bd4712c3d323e417a267da01a8aea8f3
milanb0z <bozic411@gmail.com>
2023-05-12 17:22:44 +0200

    Axios Setup & First Call

--- client/src/pages/dashboard/variants/bugs/bugs.tsx
+++ client/src/pages/dashboard/variants/bugs/bugs.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "../../../../axios.default";
+
+interface Bug {
+  date_opened: Date;
+  isOpen: boolean;
+  title: string;
+  description: string;
+  id: string;
+}
 
 const Bugs = () => {
-  return <div>Bugs</div>;
+  const [bugs, setBugs] = useState<Bug[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("/bug")
+      .then(({ data }) => {
+        setBugs(data);
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <div>
+      {bugs.map((bug) => (
+        <h2>{bug.title}</h2>
+      ))}
+    </div>
+  );
 };
 
 export default Bugs;
