48b9e19648f981d0a49f24ff9fd2a6ab67538acc
milanb0z <bozic411@gmail.com>
2023-05-12 14:14:10 +0200

    User Login

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,3 +1,66 @@
-const router = require("express").Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-module.exports = router;
+const Joi = require("joi");
+
+const userSchema = mongoose.Schema({
+  username: {
+    unique: true,
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  bio: {
+    required: true,
+    type: String,
+    default: "404 Bio Not Found",
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  date_joined: {
+    type: Date,
+    default: Date.now,
+  },
+  isVerified: { type: Boolean, default: false },
+});
+
+userSchema.set("toJSON", {
+  transform: function (doc, ret, options) {
+    ret.id = ret._id;
+    delete ret.password;
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+userSchema.methods.hashPassword = async function (password) {
+  try {
+    return await bcrypt.hash(password, 10);
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+const userValidatonSchema = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  bio: Joi.string().max(1000).required(),
+});
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = { User, userValidatonSchema };
--- server/router/user.router.js
+++ server/router/user.router.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("bug Live");
+const { User } = require("../models/user.model");
+
+//Create User
+router.post("/", async (req, res) => {
+  try {
+    const { username, email, bio, password } = req.body;
+
+    const newUser = new User({ username, email, bio });
+
+    const hashedPass = await newUser.hashPassword(password);
+
+    console.log(hashedPass);
+
+    res.send(newUser);
+  } catch (error) {
+    console.error({ error });
+    res.status(500).send([error]);
+  }
 });
 
 module.exports = router;
